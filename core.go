2828c03858c07224b3be2cdb571d50bc695a10a1
Dominik <Dominik>
2025-01-10 19:21:45 +0100

    Feat: running an executable via terminal user input with exe path

--- core.go
+++ core.go
@@ -1,9 +1,41 @@
 package main
 
 import (
+	"bufio"
 	"fmt"
+	"os"
+	"os/exec"
+	"strings"
 )
 
 func main() {
-	fmt.Println("Hello World")
+	reader := bufio.NewReader(os.Stdin)
+
+	fmt.Print("Enter the full path to exe: ")
+	input, _ := reader.ReadString('\n')
+	input = strings.TrimSpace(input)
+
+	info, err := os.Stat(input)
+	if err != nil {
+		if os.IsNotExist(err) {
+			fmt.Printf("File does not exist: %s\n", input)
+		} else {
+			fmt.Printf("Error accessing file: %s\n", input)
+		}
+	}
+	fmt.Println()
+	fmt.Printf("| %-15s: %s\n", "File Name", info.Name())
+	fmt.Printf("| %-15s: %d\n", "Size (bytes)", info.Size())
+	fmt.Printf("| %-15s: %s\n", "Permissions", info.Mode())
+	fmt.Printf("| %-15s: %s\n", "Last Modified", info.ModTime())
+	fmt.Printf("| %-15s: %v\n", "Is Directory", info.IsDir())
+	fmt.Println()
+
+	cmd := exec.Command(input)
+
+	fmt.Printf("Running %s\n", input)
+	err = cmd.Run()
+	if err != nil {
+		fmt.Printf("Error running executable: %v\n", err)
+	}
 }
