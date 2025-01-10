83d014deef921a887c82d2fe2f36ed6ba88b0d7b
Omar Hopman <omar.hopman9@gmail.com>
2025-01-10 23:04:30 +0100

    fix: combine

--- main.go
+++ main.go
@@ -1,7 +1,13 @@
 package main
 
 import (
+	"bufio"
 	"embed"
+	"fmt"
+	"os"
+	"os/exec"
+	"strings"
+	"time"
 
 	"github.com/wailsapp/wails/v2"
 	"github.com/wailsapp/wails/v2/pkg/options"
@@ -32,6 +38,62 @@ func main() {
 		println("App error: ", err.Error())
 	}
 
+	// 🐐routine
 	torrentManager := start_client()
-	go torrentManager.start_torrent("magnet:?xt=urn:btih:BB5F06D3DC020BCCDD8949E0C80DC6B2A236FE9C")
+	go func() {
+		torrent := torrentManager.start_torrent("magnet:?xt=urn:btih:BB5F06D3DC020BCCDD8949E0C80DC6B2A236FE9C")
+		ticker := time.NewTicker(1 * time.Second)
+		for {
+			select {
+			case <-ticker.C:
+				completionRatio := float64(torrent.BytesCompleted()) / float64(torrent.Info().TotalLength())
+
+				fmt.Printf("Download progress: %.2f%%\n", completionRatio*100)
+				if completionRatio >= 1.0 {
+					ticker.Stop()
+					return
+				}
+			}
+		}
+	}()
+}
+
+func select_app() {
+	reader := bufio.NewReader(os.Stdin)
+
+	fmt.Print("Enter the full path to exe: ")
+	input, _ := reader.ReadString('\n')
+	input = strings.TrimSpace(input)
+
+	// C:\Riot Games\Riot Client\RiotClientServices.exe
+	info, err := os.Stat(input)
+	if err != nil {
+		if os.IsNotExist(err) {
+			fmt.Printf("File does not exist: %s\n", input)
+		} else {
+			fmt.Printf("Error accessing file: %s\n", input)
+		}
+	}
+
+	printFileInfo(info)
+
+	cmd := exec.Command(input)
+	cmd.Stdout = os.Stdout
+	cmd.Stderr = os.Stderr
+
+	fmt.Printf("Running %s\n", input)
+	err = cmd.Run()
+	if err != nil {
+		fmt.Printf("Error running executable: %v\n", err)
+	}
+}
+
+func printFileInfo(info os.FileInfo) {
+	fmt.Println("\nFile Information:")
+	fmt.Printf("| %-20s: %-40s |\n", "File Name", info.Name())
+	fmt.Printf("| %-20s: %-40d |\n", "Size (bytes)", info.Size())
+	fmt.Printf("| %-20s: %-40s |\n", "Permissions", info.Mode())
+	fmt.Printf("| %-20s: %-40s |\n", "Last Modified", info.ModTime())
+	fmt.Printf("| %-20s: %-40v |\n", "Is Directory", info.IsDir())
+	fmt.Println()
 }
--- torrent.go
+++ torrent.go
@@ -6,18 +6,11 @@ import (
 	"github.com/anacrolix/torrent"
 )
 
-const (
-	RetrievingMetadata = 1
-	Downloading        = 2
-	Seeding            = 3
-	Stopped            = 4
-	Checking           = 5
-)
-
 type Manager struct {
 	client *torrent.Client
 }
 
+// start client en geef manager zodat je makkelijk kan bedienen zawg
 func start_client() *Manager {
 	client, err := torrent.NewClient(nil)
 	if err != nil {
@@ -27,7 +20,9 @@ func start_client() *Manager {
 	return &Manager{client: client}
 }
 
-func (manager Manager) start_torrent(magnetLink string) {
+// start torrent download
+// ik doe ook dat de torrent returned want vrijheid
+func (manager Manager) start_torrent(magnetLink string) *torrent.Torrent {
 	torrent, _ := manager.client.AddMagnet(magnetLink)
 	print("Added torrent")
 
@@ -39,4 +34,6 @@ func (manager Manager) start_torrent(magnetLink string) {
 	manager.client.WaitAll()
 
 	fmt.Println("Downloaded torrent")
+
+	return torrent
 }
