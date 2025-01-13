7d0046e2792d2a1824a205df50abfaf3fff495bd
Dominik <Dominik>
2025-01-13 20:09:55 +0100

    ui toggle download function

---
+++ frontend/src/isDownloading.js
@@ -0,0 +1,18 @@
+let isDownloading = true;
+
+function toggleDownload() {
+    const downloadItem = document.getElementById('download-item');
+    const placeholderText = document.getElementById('placeholder-text');
+
+    if (isDownloading) {
+        downloadItem.style.display = 'flex';
+        placeholderText.style.display = 'none';
+    } else {
+        downloadItem.style.display = 'none';
+        placeholderText.style.display = 'block';
+    }
+
+    isDownloading = !isDownloading;
+}
+
+// toggleDownload();
