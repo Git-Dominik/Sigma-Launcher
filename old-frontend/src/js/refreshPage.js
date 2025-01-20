bce0ed4a4226b642d912fdfdf1fb49900083e3f5
Omar Hopman <omar.hopman9@gmail.com>
2025-01-20 19:30:24 +0100

    feat: init rust

--- old-frontend/src/js/refreshPage.js
+++
@@ -1,6 +0,0 @@
-document.getElementById("refresh-page").addEventListener("click", function() {
-    setTimeout(function() {
-        location.reload();
-        console.log("reloaded");
-    });
-});
